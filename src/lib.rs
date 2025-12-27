a97c8c1d1d4dabc8a3f653dd6edfe29e665e1c2a
dastardlygames <67876084+dastardlygames@users.noreply.github.com>
2025-12-27 18:22:36 +0000

    Revert temporary lib export (remove broken lib.rs)

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Public library exports
-pub mod chart;
-pub mod widget;
-pub mod config;
-pub mod modal;
-pub mod screen;
-pub mod style;
-pub mod network;
-// Note: `data` is a separate workspace crate; it is available as the `data` dependency.
